ba02db245d1aa262fd8416c33c736547b5275671
airnesygroup <airnesy.info@gmail.com>
2025-08-20 12:26:45 +0300

    Initial commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -17,7 +17,7 @@ export const getAllProperties = async () => {
     }
     return response.data;
   } catch (error) {
-    toast.error("Something went rong");
+    toast.error("Something went wrong");
     throw error;
   }
 };
