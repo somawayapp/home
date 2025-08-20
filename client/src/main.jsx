998efb8540315ab7b3908f522e34969ab70d9288
airnesygroup <airnesy.info@gmail.com>
2025-08-20 13:52:15 +0300

    Initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,10 +13,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      authorizationParams={{
       redirect_uri: "https://homeclient.vercel.app"
      }}
-     audience="https://dev-o5a05odll5uw2ohu.au.auth0.com/api/v2/"
+     audience="https://home-teal-mu.vercel.app/api
+"
      scope="openid profile email"
     >
       <App />
     </Auth0Provider>
   </React.StrictMode>
 );
+
