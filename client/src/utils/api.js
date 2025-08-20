568b248a6b65ff84aa408d62b29a949adfea63fd
airnesygroup <airnesy.info@gmail.com>
2025-08-20 13:21:51 +0300

    Initial commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://homeserver.vercel.app/api",
+  baseURL: "https://home-teal-mu.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
