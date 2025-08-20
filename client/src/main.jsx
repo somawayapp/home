c36fe5e0d25d1fcc80ff3b4615df9f62bdf835c4
airnesygroup <airnesy.info@gmail.com>
2025-08-20 13:10:39 +0300

    Initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
-     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+     domain="dev-o5a05odll5uw2ohu.au.auth0.com"
+     clientId="vtGPFtLS2dKG6aUMv0lvgaFMqJskypbC"
      authorizationParams={{
-      redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app"
+      redirect_uri: "https://homeclient.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,9 +1,9 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://dev-o5a05odll5uw2ohu.au.auth0.com/api/v2/",
     issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
 
-export default jwtCheck
+export default jwtCheck  
