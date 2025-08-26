08aaf1c5c8d197a8273a78ba0f743e2a6326e68f
airnesygroup <airnesy.info@gmail.com>
2025-08-26 23:05:59 +0300

    Initial commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -111,7 +111,7 @@ const Navbar = () => {
 
   {open && (
     <div
-      className="fixed inset-0 bg-black bg-opacity-40 z-30"
+      className="fixed inset-0 bg-black/20 z-30"
       onClick={() => setOpen(false)} // clicking backdrop closes menu
     ></div>
   )}
