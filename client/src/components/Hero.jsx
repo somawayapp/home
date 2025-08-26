836586d76b924ac5508e1af0442f84ac0c8e2f76
airnesygroup <airnesy.info@gmail.com>
2025-08-26 20:59:12 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -34,20 +34,16 @@ const Hero = () => {
   }
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-      className='flex flex-col items-center justify-center bg-light text-center'
-    >
+  
       <motion.form
         initial={{ scale: 0.95, opacity: 0, y: 50 }}
         animate={{ scale: 1, opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.4 }}
         onSubmit={handleSearch}
-        className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'
+        className='flex flex-row items-center justify-between p-6 rounded-full w-full max-w-80
+         md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'
       >
-        <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
+        <div className='flex flex-row items-center gap-10 min-md:ml-8'>
           {/* Pickup Location */}
           <div className='flex flex-col items-start gap-2'>
             <select required value={pickupLocation} onChange={(e) => setPickupLocation(e.target.value)}>
@@ -95,13 +91,11 @@ const Hero = () => {
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer'
+          className='flex items-center justify-center gap-1 px-3 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer'
         >
-          <img src={assets.search_icon} alt='search' className='brightness-300' />
-          Search
+          <img src={assets.search_icon} alt='search' className=' brightness-300' />
         </motion.button>
       </motion.form>
-    </motion.div>
   )
 }
 
