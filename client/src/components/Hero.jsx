9f8fb087d5ea31adafc52f6501b364bb2eef0c4d
airnesygroup <airnesy.info@gmail.com>
2025-08-26 23:27:37 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -63,21 +63,21 @@ const Hero = () => {
     <>
       {/* Desktop / Tablet View */}
 
-      <div className='flex flex-col  '>
-       <div className="flex flex-row gap-8">
+      <div className='flex flex-col items-center justify-center '>
+       <div className="flex flex-row gap-15">
       {links.map((link) => {
         const isActive = currentPath === link.path
         return (
           <Link
             key={link.path}
             to={link.path}
-            className={`relative font-semibold text-gray-700 transition-colors 
-              hover:text-primary
-              ${isActive ? "text-primary" : ""}`}
+            className={`relative  text-gray-700 transition-colors 
+              hover:text-black
+              ${isActive ? "text-black" : ""}`}
           >
             {link.name}
             {isActive && (
-              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-primary rounded-full"></span>
+              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-black rounded-full"></span>
             )}
           </Link>
         )
