f5a937aa64611ab00f18dc93b44f3a82cb8d872a
airnesygroup <airnesy.info@gmail.com>
2025-08-27 13:06:41 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -130,7 +130,7 @@ const Hero = () => {
           className="flex items-center justify-between w-full max-w-120 gap-2 bg-white rounded-full text-gray-600
             shadow-[0px_8px_20px_rgba(0,0,0,0.1)] border border-light mt-4  md:mt-3"
         >
-          <div className="flex-1 flex justify-between items-center text-md md:text-sm">
+          <div className="flex-1 flex justify-between items-center text-md md:text-md">
             <span className="px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer">
               {pickupLocation || "Any location"}
             </span>
