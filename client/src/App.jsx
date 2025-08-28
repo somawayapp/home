1aa0094487dce61caba85eb7d973d929720d25cb
airnesygroup <airnesy.info@gmail.com>
2025-08-28 12:24:21 +0300

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
-import Footer from './components/Footer'
 import Layout from './pages/owner/Layout'
 import Dashboard from './pages/owner/Dashboard'
 import AddCar from './pages/owner/AddCar'
@@ -14,34 +12,33 @@ import ManageBookings from './pages/owner/ManageBookings'
 import Login from './components/Login'
 import { Toaster } from 'react-hot-toast'
 import { useAppContext } from './context/AppContext'
+import SiteLayout from './components/SiteLayout'
 
 const App = () => {
-
-  const {showLogin} = useAppContext()
-  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+  const { showLogin } = useAppContext()
 
   return (
     <>
-     <Toaster />
-      {showLogin && <Login/>}
+      <Toaster />
+      {showLogin && <Login />}
 
-      {!isOwnerPath && <Navbar/>}
+      <Routes>
+        {/* Public site layout */}
+        <Route element={<SiteLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/car-details/:id" element={<CarDetails />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+        </Route>
 
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/car-details/:id' element={<CarDetails/>}/>
-      <Route path='/cars' element={<Cars/>}/>
-      <Route path='/my-bookings' element={<MyBookings/>}/>
-      <Route path='/owner' element={<Layout />}>
-        <Route index element={<Dashboard />}/>
-        <Route path="add-car" element={<AddCar />}/>
-        <Route path="manage-cars" element={<ManageCars />}/>
-        <Route path="manage-bookings" element={<ManageBookings />}/>
-      </Route>
-    </Routes>
-
-    {!isOwnerPath && <Footer />}
-    
+        {/* Owner dashboard layout */}
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-car" element={<AddCar />} />
+          <Route path="manage-cars" element={<ManageCars />} />
+          <Route path="manage-bookings" element={<ManageBookings />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ client/src/components/SiteLayout.jsx
@@ -0,0 +1,24 @@
+// src/components/SiteLayout.jsx
+import React from "react"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import { Outlet } from "react-router-dom"
+import { useAppContext } from "../context/AppContext"
+
+const SiteLayout = () => {
+  const { showLogin } = useAppContext()
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+
+      <main className="flex-1">
+        <Outlet /> {/* This renders the page content */}
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default SiteLayout
