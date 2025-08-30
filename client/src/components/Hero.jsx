295039f96de51f9432a8305b38dbf3398ade3239
airnesygroup <airnesy.info@gmail.com>
2025-08-30 20:44:07 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -196,7 +196,7 @@ useEffect(() => {
     <div className='flex flex-col items-center justify-center'>
 
 
-<div className="hidden md:flex flex-row gap-10 ml-2">
+<div className="hidden md:flex flex-row gap-10 ml-4">
   
 
   {links.map((link) => {
