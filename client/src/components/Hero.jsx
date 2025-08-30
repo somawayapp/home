3336e3ab0f772353377564840bbba82673c42ec3
airnesygroup <airnesy.info@gmail.com>
2025-08-30 21:26:48 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -160,7 +160,7 @@ useEffect(() => {
   }
 
   return (
-    <div className="flex  flex-col bg-white transition-colors duration-300 px-4 md:px-12 items-center justify-between 
+    <div className="flex border border-bottom border-light flex-col bg-white transition-colors duration-300 px-4 md:px-12 items-center justify-between 
     w-full sticky top-0 left-0 right-0 z-50">
 
       {/* === Desktop / MD+ form (expanded) */}
@@ -195,7 +195,7 @@ useEffect(() => {
   {/* === Links Section === */}
 
    {!isSmallScreen && showDesktop && location.pathname === "/" && (
-    <div className='flex flex-col mt-5 items-center justify-center'>
+    <div className='flex flex-col mt-5 mb-4 items-center justify-center'>
 
 
 <div className="hidden md:flex  flex-row gap-10 mb-2 ml-4">
@@ -317,7 +317,7 @@ useEffect(() => {
     gap-1 md:gap-4
     bg-white rounded-full text-gray-600
     shadow-[0px_8px_20px_rgba(0,0,0,0.1)] border border-light
-    mt-4 md:mt-3 max-w-150
+    mt-5 md:mt-6 max-w-150
   "
 >
   <div className="flex-1 gap-1 md:gap-4 flex justify-between items-center">
