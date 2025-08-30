9fa5c3e84993ca4d696a61405b4666aa002470b3
airnesygroup <airnesy.info@gmail.com>
2025-08-30 11:23:28 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -160,7 +160,7 @@ useEffect(() => {
   }
 
   return (
-    <div className="flex  transition-colors duration-300 px-4 md:px-12 items-center justify-between 
+    <div className="flex bg-white transition-colors duration-300 px-4 md:px-12 items-center justify-between 
     w-full sticky top-0 left-0 right-0 z-50">
 
       {/* === Desktop / MD+ form (expanded) */}
@@ -359,7 +359,7 @@ useEffect(() => {
 
 
         <div  onSubmit={handleSearch}
-          className="hidden md:flex mt-5 mb-5 flex-row items-center justify-between rounded-full w-full  bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)] border border-light"
+          className="hidden md:flex mt-5 mb-5 flex-row items-center justify-between rounded-full  bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)] border border-light"
         >
           <div className="flex flex-row items-center gap-9 ml-4" onClick={() => setShowModal(true)}>
             <div className="flex flex-col px-6 mr-9 items-start rounded-full hover:bg-gray-100 transition-colors cursor-pointer">
@@ -418,11 +418,11 @@ useEffect(() => {
           
   onClick={() => setShowModal(true)}
   className="
-    flex items-center justify-between w-full
+    flex items-center justify-between
     gap-1 md:gap-4
     bg-white rounded-full text-gray-600
     shadow-[0px_8px_20px_rgba(0,0,0,0.1)] border border-light
-    mt-4 md:mt-3 max-w-140 md:max-w-450
+    mt-4 md:mt-3 max-w-150
   "
 >
   <div className="flex-1 gap-1 md:gap-4 flex justify-between items-center">
