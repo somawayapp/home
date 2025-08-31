215219d606d6e7b4801c8d1c5c7747d94c1656ac
airnesygroup <airnesy.info@gmail.com>
2025-08-31 12:07:36 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -338,7 +338,7 @@ useEffect(() => {
                   changeRole()
                 }
               }}
-              className='cursor-pointer  rounded-3xl px-4 py-2 transition hover:bg-bgColorhover'
+              className='cursor-pointer  rounded-3xl px-4 py-2 transition hover:bg-bgColor'
             >
               {isOwner ? 'Dashboard' : 'Add listing'}
             </button>
@@ -481,7 +481,7 @@ useEffect(() => {
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className="flex items-center justify-center gap-1 px-4 py-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
+              className="flex items-center justify-center gap-1 px-3 py-3 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
             >
               <img src={assets.search_icon} alt="search" className="brightness-300 md:h-4 md:w-4" />
             </motion.button>
