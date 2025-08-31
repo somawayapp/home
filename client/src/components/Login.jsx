6ade6116b7ee62dc90b222ad4650f5f508d004cd
airnesygroup <airnesy.info@gmail.com>
2025-08-31 23:13:43 +0300

    Initial commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -131,7 +131,7 @@ const Login = () => {
           <button
             type="button"
             onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute right-3 top-8 text-gray-400 hover:text-gray-600"
+            className="absolute hover-cursor right-3 top-8 text-gray-400 hover:text-gray-600"
           >
             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
           </button>
