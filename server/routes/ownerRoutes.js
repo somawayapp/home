de1a6a8be8c9a54c0f82e97fb7da0cd7c9758ec0
airnesygroup <airnesy.info@gmail.com>
2025-08-31 20:29:14 +0300

    Initial commit

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -6,7 +6,7 @@ import upload from "../middleware/multer.js";
 const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
-ownerRouter.post("/add-car", upload.single("image"), protect, addCar)
+ownerRouter.post("/add-car", upload.array("images", 20), protect, addCar)
 ownerRouter.get("/cars", protect, getOwnerCars)
 ownerRouter.post("/toggle-car", protect, toggleCarAvailability)
 ownerRouter.post("/delete-car", protect, deleteCar)
