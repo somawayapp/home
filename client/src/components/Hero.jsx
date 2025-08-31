cabd925cc9068d5c67272e364d2109cb671ba375
airnesygroup <airnesy.info@gmail.com>
2025-08-31 12:21:08 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -453,27 +453,27 @@ useEffect(() => {
         >
           <div className="flex flex-row items-center " onClick={() => setShowModal(true)}>
             <div className="flex flex-col py-3 pr-29  pl-9 items-start rounded-full hover:bg-gray-100 transition-colors cursor-pointer">
-              <p className="text-sm font-sm text-dark">Any where</p>
-              <p className=" text-sm text-textlight">{pickupLocation || ' search destinations'}</p>
+              <p className="text-sm font-sm text-dark">Where</p>
+              <p className=" text-15 text-textlight">{pickupLocation || ' Search destinations'}</p>
             </div>
 
             <span className="h-9 w-px bg-borderColor"></span>
             <div className="flex flex-col py-3 px-8 items-start rounded-full hover:bg-gray-100 transition-colors cursor-pointer">
-              <p className="text-sm font-sm text-dark">Any price</p>
-              <p className="text-sm text-textlight">{pricePerDay || 'Enter max price '}</p>
+              <p className="text-sm font-sm text-dark"> Price</p>
+              <p className="text-15 text-textlight">{pricePerDay || 'Enter max price '}</p>
             </div>
 
              <span className="h-9 w-px bg-borderColor"></span>
              <div className="flex flex-col py-3 px-8 items-start rounded-full hover:bg-gray-100 transition-colors cursor-pointer">
-              <p className="text-sm font-sm text-dark">Any type</p>
-              <p className=" text-sm text-textlight">{pickupLocation || ' select type'}</p>
+              <p className="text-sm font-sm text-dark">Type</p>
+              <p className=" text-15 text-textlight">{pickupLocation || ' Select type'}</p>
             </div>
 
 
             <span className="h-9 w-px bg-borderColor"></span>
             <div className="flex flex-col py-3 px-8 items-start rounded-full hover:bg-gray-100 transition-colors cursor-pointer">
-              <p className="text-sm font-sm text-textdark">Any size</p>
-              <p className="text-sm text-textlight">{seatingCapacity || 'Enter size'}</p>
+              <p className="text-sm font-sm text-textdark">Size</p>
+              <p className="text-15 text-textlight">{seatingCapacity || 'Enter size'}</p>
             </div>
           </div>
 
