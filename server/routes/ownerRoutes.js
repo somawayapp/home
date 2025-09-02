c30a38d5e92f44ddb1125a344939e9c5560e235c
airnesygroup <airnesy.info@gmail.com>
2025-09-02 10:13:49 +0300

    Initial commit

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,15 +1,13 @@
-import express from "express";
 import { protect } from "../middleware/auth.js";
 import { addListing, changeRoleToOwner, getAuthenticationParameters, deleteCar, getDashboardData, getOwnerListings, toggleCarAvailability, updateUserImage } from "../controllers/ownerController.js";
 import upload from "../middleware/multer.js";
-import { verifyToken } from '../middleware/auth.js';
 
 const ownerRouter = express.Router();
 
 // New route for ImageKit authentication
 ownerRouter.get('/imagekit-auth', getAuthenticationParameters);
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
-ownerRouter.post('/add-listing', verifyToken, addListing);
+ownerRouter.post('/add-listing', protect, addListing);
 ownerRouter.get("/listings", protect, getOwnerListings)
 ownerRouter.post("/toggle-car", protect, toggleCarAvailability)
 ownerRouter.post("/delete-car", protect, deleteCar)
