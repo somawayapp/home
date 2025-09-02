480245d1323d5d3826e2963841a5f276a03f01b6
airnesygroup <airnesy.info@gmail.com>
2025-09-02 10:07:20 +0300

    Initial commit

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -8,11 +8,7 @@ import path from "path";
 import pLimit from "p-limit";
 
 
-const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-});
+
 
 
 
