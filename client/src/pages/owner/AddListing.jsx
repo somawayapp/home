c2fbcbb27aab04a195246a03c7390677bf3d0065
airnesygroup <airnesy.info@gmail.com>
2025-09-09 09:22:16 +0300

    Initial commit

--- client/src/pages/owner/AddListing.jsx
+++ client/src/pages/owner/AddListing.jsx
@@ -778,7 +778,6 @@ useEffect(() => {
                     ))}
                   </div>
                 </div>
-                <div>
 
 
                 <div className="border-2 p-2 md:p-6  border-bgColor   hover:border-bgColorhover " >
@@ -799,6 +798,7 @@ useEffect(() => {
                     ))}
                   </div>
                   </div>
+                <div>
 
                 </div>
 
