062baed0c3c525bae2bfa2c4840a03a6937530b7
airnesygroup <airnesy.info@gmail.com>
2025-10-02 12:20:46 +0300

    Initial commit

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -74,7 +74,7 @@ const ListingCard = ({ listing }) => {
 
           {/* ✅ Status Badge */}
           <div className={`absolute rounded-full bottom-3 right-3 px-3 py-1 text-white text-sm font-medium
-            ${listing.listingstatus ? "bg-[#2F74FD] hover:bg-[#0556f7]" : "FFD700"}`}>
+            ${listing.listingstatus ? "bg-[#2F74FD] hover:bg-[#0556f7]" : "#FFD700"}`}>
             {listing.listingstatus ? "Available" : "Unavailable"}
           </div>
 
