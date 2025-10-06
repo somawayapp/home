9648ec31197d2546593fdf6dfb2091dc99bd261b
airnesygroup <airnesy.info@gmail.com>
2025-10-06 18:55:09 +0300

    Initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -475,15 +475,18 @@ useEffect(() => {
         Array.from({ length: 8 }).map((_, index) => (
           <div
             key={index}
-            className="animate-pulse bg-gray-100 rounded-2xl shadow-sm overflow-hidden"
+            className="gap-4 flex flex-col"
           >
-            <div className="h-48 bg-gray-200" />
+            <div className="animate-pulse bg-gray-100 rounded-2xl shadow-sm overflow-hidden">
+                      </div>
+
             <div className="p-4 space-y-3">
               <div className="h-4 bg-gray-200 rounded w-3/4"></div>
               <div className="h-4 bg-gray-200 rounded w-1/2"></div>
               <div className="h-4 bg-gray-200 rounded w-1/3"></div>
             </div>
-          </div>
+                      </div>
+
         ))}
 
       {/* --- Listings --- */}
