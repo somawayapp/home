10cf5e42ea5ce26653c893bf3ab3d07b8e7c0a6f
airnesygroup <airnesy.info@gmail.com>
2025-10-28 16:42:24 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -166,9 +166,9 @@ useEffect(() => {
 
 
   return (
-    <div className="flex border border-bottom border-light flex-col bg-white transition-colors 
+    <div className="flex  border-b border-light flex-col bg-white transition-colors 
     duration-300 px-4 md:px-16 items-center justify-between 
-    w-full sticky top-0 left-0 right-0 z-50 py-4 "
+    w-full sticky top-0 left-0 right-0 z-50 py-6 "
   >
 
       {/* === Desktop / MD+ form (expanded) */}
