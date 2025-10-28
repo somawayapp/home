356d396f784fe0e5763d877cf7f9fdb6e090d076
airnesygroup <airnesy.info@gmail.com>
2025-10-28 20:51:34 +0300

    Initial commit

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -67,7 +67,7 @@ export default function CategoryBar({ filters, setFilters, getActiveFilters, han
 
   return (
     <div className="relative pt-4 mb-4 group ">
-      <div className="relative px-4   md:px-30   flex items-center">
+      <div className="relative px-4  md:px-30  flex items-center">
         {/* Scroll container */}
         <div
           ref={scrollRef}
@@ -98,7 +98,15 @@ export default function CategoryBar({ filters, setFilters, getActiveFilters, han
 
         {/* Filters button + popup wrapper */}
         <div className="relative flex-shrink-0 flex flex-row ml-2 gap-4">
-       
+          {/* Right arrow */}
+          {canScrollRight && (
+            <button
+              onClick={() => scroll("right")}
+              className="absolute -left-12 top-1/2 -translate-y-1/2 bg-white text-black border border-borderColor  p-2 rounded-full cursor-pointer shadow-md hover:shadow-xl hover:bg-white/60  transition"
+            >
+              <ChevronRight size={22} />
+            </button>
+          )}
 
           {/* Filters button */}
           <button
@@ -106,6 +114,7 @@ export default function CategoryBar({ filters, setFilters, getActiveFilters, han
             className="flex items-center gap-2 border border-borderColor py-4 px-4 md:px-6 rounded-full md:rounded-xl hover:shadow-xl hover:text-neutral-900 shadow-lg text-neutral-700 hover:border-borderColorhover cursor-pointer transition"
           >
             <FaSlidersH />
+            <span className="hidden sm:inline"> Filters</span>
           </button>
 
           {/* Popup */}
@@ -160,7 +169,15 @@ export default function CategoryBar({ filters, setFilters, getActiveFilters, han
         </div>
       </div>
 
-    
+      {/* Left arrow */}
+      {canScrollLeft && (
+        <button
+          onClick={() => scroll("left")}
+          className="absolute hidden md:flex left-16 top-1/2 -translate-y-1/2  bg-white text-black border border-borderColor p-2 rounded-full cursor-pointer shadow-md hover:shadow-xl hover:bg-white/60 transition"
+        >
+          <ChevronLeft size={22} />
+        </button>
+      )}
     </div>
   );
 }
