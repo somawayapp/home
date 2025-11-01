55f63e60d24fa04c82227273bb920edfcdb793bf
airnesygroup <airnesy.info@gmail.com>
2025-11-01 21:08:01 +0300

    Initial commit

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -50,7 +50,7 @@ const ListingCard = ({ listing }) => {
     <div className="relative gap-2 md:gap-4 group mb-2 md:mb-[8px] overflow-hidden ">
       {/* ✅ Updated Link to generic listing page */}
       <Link to={`/listing-details/${listing._id}`} className="block">
-        <div className="relative w-full h-full aspect-[3/2.9] rounded-2xl overflow-hidden">
+        <div className="relative w-full h-full aspect-[3/2.9] rounded-3xl overflow-hidden">
           <div
             ref={scrollRef}
             onScroll={handleScroll}
@@ -61,7 +61,7 @@ const ListingCard = ({ listing }) => {
                 <img
                   key={index}
                   src={image}
-                  className="w-full h-full bg-bgColor object-cover rounded-2xl flex-shrink-0 snap-center"
+                  className="w-full h-full bg-bgColor object-cover rounded-3xl flex-shrink-0 snap-center"
                   alt={`image-${index}`}
                 />
               ))
