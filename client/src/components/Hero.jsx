89408cc38b9bd47d1e6dee6b6be9294eddec0ef5
airnesygroup <airnesy.info@gmail.com>
2025-11-24 11:31:20 +0300

    Initial commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -225,7 +225,7 @@ useEffect(() => {
     C981.259 737.675 958.638 687.769 949.278 666.715
   " fill="none" stroke="currentColor" stroke-width="90" stroke-linecap="round" stroke-linejoin="round"/>
 
-  <text x="1200" y="650" font-family="Arial, sans-serif" font-size="800" font-weight="550" fill="currentColor" dominant-baseline="middle">hodii</text>
+  <text x="1200" y="650" font-family="Arial, sans-serif" font-size="800" font-weight="530" fill="currentColor" dominant-baseline="middle">hodii</text>
 </svg>
 
 </div>
